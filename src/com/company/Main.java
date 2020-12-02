f50c58df5d52d4d6d8fca43e3096d1d8943b94ac
Islambek-kun-kg <72745619+Islambek-kun-kg@users.noreply.github.com>
2020-12-03 00:54:52 +0600

    Add files via upload

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,22 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Начало эстафеты!");
-        for (int i = 1; i < 5; i++) {
-            Runner runner = new Runner("Runner ", i,1);
+        int j = -9;
+        for (int i = 1; i < 8; i++) {
+            Runner runner = new Runner("Runner ", i, 1);
+            runner.setConstanta(j = j + 2);
             try {
                 runner.start();
                 runner.join();
             } catch (InterruptedException e) {
                 e.printStackTrace();
-            }
-        }
-        for (int i = 5; i > 1; i--) {
-            Runner runner = new Runner("Runner ", i,2);
-            try {
-                runner.start();
-                runner.join();
-            } catch (InterruptedException r) {
-                r.printStackTrace();
             }
         }
     }
--- src/com/company/Runner.java
+++ src/com/company/Runner.java
@@ -5,40 +5,50 @@ public class Runner extends Thread {
     private int number;
     private int constanta;
 
+    public void run() {
+        if (number > 4) {
+            if (number == 5)
+                System.out.println(getName() + number + " бежит к " + getName() + (number - constanta));
+            if ((number - constanta) != 5)
+                System.out.println(getName() + (number - constanta) + " берет палочку");
+            System.out.println(getName() + (number - constanta) + " бежит к " + getName() + (number - constanta - 1));
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                if ((number - constanta) == 2) {
+                    System.out.println(getName() + (number - constanta - 1) + " берет палочку");
+                }
+            }
+        } else {
+            System.out.println(getName() + number + " берет палочку");
+            System.out.println(getName() + number + " бежит к " + getName() + (number + 1));
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                if (this.number == 4) {
+                    System.out.println(getName() + (number + 1) + " берет палочку");
+                    System.out.println(getName() + (number + 1) + " бежит к финишу");
+                }
+            }
+        }
+    }
+
     public Runner(String name, int number, int constanta) {
         super(name);
         this.number = number;
         this.constanta = constanta;
     }
 
-    public void run() {
-        if (constanta == 1) {
-            System.out.println(this.getName() + this.number + " берет палочку");
-            System.out.println(this.getName() + this.number + " бежит к " + this.getName() + (this.number + 1));
-            try {
-                sleep(5000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } finally {
-                if (this.number == 4) {
-                    System.out.println(this.getName() + (this.number + 1) + " берет палочку");
-                    System.out.println(this.getName() + (this.number + 1) + " бежит к финишу");
-                }
-            }
-        }
-        if (constanta == 2) {
-            if (this.number != 5)
-                System.out.println(this.getName() + this.number + " берет палочку");
-            System.out.println(this.getName() + this.number + " бежит к " + this.getName() + (this.number - 1));
-            try {
-                sleep(5000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } finally {
-                if (this.number == 2) {
-                    System.out.println(this.getName() + (this.number - 1) + " берет палочку");
-                }
-            }
-        }
+    public int getConstanta() {
+        return constanta;
     }
+
+    public void setConstanta(int constanta) {
+        this.constanta = constanta;
+    }
+
 }
