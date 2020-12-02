7cee46f1a8709d26be7b2e4eeb293d5514e1c32e
Islambek-kun-kg <72745619+Islambek-kun-kg@users.noreply.github.com>
2020-12-03 00:08:38 +0600

    Add files via upload

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Начало эстафеты!");
         for (int i = 1; i < 5; i++) {
-            Runner runner = new Runner("Runner ", i);
+            Runner runner = new Runner("Runner ", i,1);
             try {
                 runner.start();
                 runner.join();
@@ -13,6 +13,14 @@ public class Main {
                 e.printStackTrace();
             }
         }
-
+        for (int i = 5; i > 1; i--) {
+            Runner runner = new Runner("Runner ", i,2);
+            try {
+                runner.start();
+                runner.join();
+            } catch (InterruptedException r) {
+                r.printStackTrace();
+            }
+        }
     }
 }
--- src/com/company/Runner.java
+++ src/com/company/Runner.java
@@ -3,45 +3,60 @@ package com.company;
 public class Runner extends Thread {
 
     private int number;
+    private int constanta;
 
-    public Runner(String name, int number) {
+    public Runner(String name, int number, int constanta) {
         super(name);
         this.number = number;
+        this.constanta = constanta;
     }
 
     public void run() {
-        System.out.println(this.getName() + this.number + " берет палочку");
-        System.out.println(this.getName() + this.number + " бежит к " + this.getName() + (this.number + 1));
-        try {
-            sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            if (this.number == 4) {
-                System.out.println(this.getName() + (this.number + 1) + " берет палочку");
-                System.out.println(this.getName() + (this.number + 1) + " бежит к финишу");
+        if (constanta == 1) {
+            System.out.println(this.getName() + this.number + " берет палочку");
+            System.out.println(this.getName() + this.number + " бежит к " + this.getName() + (this.number + 1));
+            try {
+                sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                if (this.number == 4) {
+                    System.out.println(this.getName() + (this.number + 1) + " берет палочку");
+                    System.out.println(this.getName() + (this.number + 1) + " бежит к финишу");
+                }
             }
         }
-        if (this.number == 4) {
-            for (int i = this.number + 1; i > 0; i--) {
-                if (i == 1) {
-                    System.out.println(this.getName() + i + " берет палочку");
-                    break;
+        if (constanta == 2) {
+           /*if (this.number == 4) {
+                for (int i = this.number + 1; i > 0; i--) {
+                    if (i == 1) {
+                        System.out.println(this.getName() + i + " берет палочку");
+                        break;
+                    }
+                    if (i == 5) {
+                        System.out.println(this.getName() + i + " бежит к " + this.getName() + (i - 1));
+                    } else {
+                        System.out.println(this.getName() + i + " берет палочку");
+                        System.out.println(this.getName() + i + " бежит к " + this.getName() + (i - 1));
+                    }
+                    try {
+                        sleep(1000);
+                    } catch (InterruptedException r) {
+                        r.printStackTrace();
+                    }
                 }
-
-                if (i == 5) {
-                    System.out.println(this.getName() + i + " бежит к " + this.getName() + (i - 1));
-                } else {
-                    System.out.println(this.getName() + i + " берет палочку");
-                    System.out.println(this.getName() + i + " бежит к " + this.getName() + (i - 1));
+            }*/
+            if (this.number != 5)
+                System.out.println(this.getName() + this.number + " берет палочку");
+            System.out.println(this.getName() + this.number + " бежит к " + this.getName() + (this.number - 1));
+            try {
+                sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                if (this.number == 2) {
+                    System.out.println(this.getName() + (this.number - 1) + " берет палочку");
                 }
-                try {
-                    sleep(5000);
-                } catch (InterruptedException r) {
-                    r.printStackTrace();
-                }
-
-
             }
         }
     }
